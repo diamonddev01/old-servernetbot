f2ca56ea1e9d004b12183cf179d18f392b036230
Oreo <thediamondgamer331@gmail.com>
2022-07-23 00:00:23 +0100

    Add more types

---
+++ src/types/channelOptions.ts
@@ -0,0 +1,32 @@
+export interface ChannelOptions {
+    id: string;
+    webhook?: boolean;
+    wh?: wh;
+    warns?: number;
+    warnings?: Warn[];
+}
+
+export interface wh {
+    id: string;
+    token: string;
+}
+
+export interface WebhookEnabledChannel {
+    id: string;
+    webhook: true;
+    wh: wh;
+}
+
+export interface WebhookDisabledChannel {
+    id: string;
+    webhook: false;
+}
+
+export interface Warn {
+    id: string;
+    reason: string;
+    time: number;
+    type?: ChannelWarnTypes;
+}
+
+type ChannelWarnTypes = 'WebhookGetFail' | 'WebhookSendFail' | 'ChannelGetFail' | 'ChannelSendFail';
