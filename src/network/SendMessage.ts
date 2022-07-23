23555ae44cb4881253c897a6bc7dad80d9e48fa6
Oreo <thediamondgamer331@gmail.com>
2022-07-23 06:11:56 +0100

    "small" update to the scan function

--- src/network/SendMessage.ts
+++ src/network/SendMessage.ts
@@ -1,12 +1,67 @@
-import { Message } from "discord.js";
+import { ChannelType, Guild, Message, User } from "discord.js";
 import { Client } from "../modified";
 import { NetworkSend } from "./__send";
 import { scan } from "./functions/scanMessage";
 import { NetworkUser } from "../classes/Network_User";
+import { SLOWMODE, SLOWMODE_TIME } from "../config";
+import { NetworkGuild } from "../classes/Network_Guild";
+import { NetworkChannel } from "../classes/Network_Channel";
 
-export function CustomNetworkMessage(client: Client, message: Message) {
-    const user = new NetworkUser(message.author);
-    const cnt = message.content;
+export async function CustomNetworkMessage(client: Client, message: Message) {
+    if (message.channel.type !== ChannelType.GuildText) return; // Fix compiler errors
+    if (!message.guild) return; // Fix compiler errors
+
+    const user = new NetworkUser(message.author); // Create a new user object
+    const guild = new NetworkGuild(message.guild); // Create a new guild object
+    const channel = new NetworkChannel(message.channel) // Create a new channel object
+    const cnt = message.content; // Move the message content into the cnt variable
+
+    const scanned = await scan(message); // Check to see if any of the content violates content policies
+    const timed = SLOWMODE ? (user.lastMessage) - Date.now() > SLOWMODE_TIME : false; // Check to see if the user is in slowmode
+    const banned = user.banned; // Checks if the user is banned
+    const GuildBanned = guild.banned; // Checks if the guild is banned
+
+    if (message.author.bot) return // Dont allow bots to send messages
+    
+    if (banned || GuildBanned) {
+        // Return the 'error'
+        message.channel.send({ content: bannedStatement(message.author, message.guild, banned ? 'U' : 'G', banned ? user : guild) }).catch(console.log);
+        message.react('❌').catch(console.log);
+        message.react('🔨').catch(console.log);
+        return;
+    }
+
+    if (timed) {
+        // Return the 'error'
+        message.channel.send({ content: `${message.author} You are in slowmode. Please wait ${((Date.now() - user.lastMessage) - SLOWMODE_TIME) * -1}ms before sending another message.` }).catch(console.log);
+        message.react('❌').catch(console.log);
+        message.react('⏲️').catch(console.log);
+        return;
+    }
+
+    if (scanned.phrases == 0) {
+        // Warn the user
+        const r = user.warnUser('Use of a banned phrase', null);
+        message.react('❌').catch(console.log);
+
+        if (r == 1) {
+            // Return the 'error'
+            message.channel.send({ content: `${message.author} You have been warned for using a banned phrase.` }).catch(console.log);
+        }
+
+        if(r == 2) {
+            // Return the 'error'
+            message.channel.send({ content: `${message.author} You have been temp-banned for using a banned phrase. | Warning escalation policy mandates that after your x warn you must be temp-banned.` }).catch(console.log);
+        }
+    }
 
     //NetworkSend(client, message, data, webhookOpts);
+}
+
+function bannedStatement(user: User, guild: Guild, banned: 'G' | 'U', bannedItem: NetworkGuild | NetworkUser): string {
+    if(banned === 'G') {
+        return `${guild.name} is banned from using servernet for ${bannedItem.ban?.reason || 'no reason'}. Please ask the server owner nicely to submit a server appeal.`;
+    }
+
+    return `${user.username}, you banned from using servernet for ${bannedItem.ban?.reason || 'no reason'}. Please submit an appeal.`;
 }
--- src/network/functions/scanMessage.ts
+++ src/network/functions/scanMessage.ts
@@ -1,24 +1,64 @@
+import { Message } from 'discord.js';
 import badPhrases from '../badPhrases/badPhrases.json';
-const { nsfw, mid, minor, disallow } = badPhrases;
+import { AUTOMOD_DEFINE_LINK, AUTOMOD_ENABLED, AUTOMOD_MAX_LENGTH } from '../../config';
+import { get } from 'https';
 
-export function scan(content: string) {
-    for (const term of disallow) {
-        if (content.includes(term)) return 0;
-    }
+export function scan_t(content: string) {
+    content = content.toLowerCase();
+    if (badPhrases.disallow.some(v => content.includes(v))) return 0;
+    if (badPhrases.nsfw.some(v => content.includes(v))) return 1;
+    if (badPhrases.minor.some(v => content.includes(v))) return 2;
+    if (badPhrases.mid.some(v => content.includes(v))) return 3;
 
-    for (const term of nsfw) {
-        if (content.includes(term)) return 1;
-    }
+    return 10;
+}
 
-    for (const term of mid) {
-        if (content.includes(term)) return 2;
-    }
+export function scan(message: Message): Promise<ScanData> {
+    return new Promise(async (res, rej) => {
+        // Detect Links
+        const content = message.content.toLowerCase();
+        const link = AUTOMOD_DEFINE_LINK.some(v => content.includes(v));
 
-    for (const term of minor) {
-        if (content.includes(term)) return 3;
-    }
+        // Detect pings
+        let ping = false;
+        ping = message.mentions.users.size > 0;
+        ping = ping ? true : message.mentions.roles.size > 0;
+        ping = ping ? true : message.mentions.everyone;
+        ping = ping ? true : message.content.includes("@here");
+        ping = ping ? true : message.content.includes("@everyone");
 
-    return 4;
+        // Detect bad links
+        const badLink = await badLinkCheck(content);
+
+        let data = {
+            link,
+            allow_length: message.content.length <= AUTOMOD_MAX_LENGTH,
+            phrases: scan_t(message.content),
+            ping,
+            badLink
+        }
+
+        res(AUTOMOD_ENABLED ? data : { link: false, allow_length: true, phrases: 10, ping: false, badLink: false });
+    });
+}
+
+// External to help https
+function badLinkCheck(content: string): Promise<boolean> {
+    return new Promise(async (resolve) => {
+        get(`https://badlink.diamondstudios.tk/discord?message="${content}"`, res => {
+            res.on('data', d => {
+                resolve(JSON.parse(d));
+            })
+        })
+    })
+}
+
+interface ScanData {
+    link: boolean;
+    allow_length: boolean;
+    phrases: number;
+    ping: boolean;
+    badLink: boolean;
 }
 
 /*
