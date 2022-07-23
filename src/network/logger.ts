0077525d4e8f699b4cc4ff5aea04723cce8b26c1
Oreo <thediamondgamer331@gmail.com>
2022-07-23 05:48:58 +0100

    Add automoderator support

--- src/network/logger.ts
+++ src/network/logger.ts
@@ -17,56 +17,66 @@ export class Logger {
             return;
         }
 
+        const moderator = warning.moderatorID ? warning.moderatorID : "829383622439206993"
+
         channel.send({
-            content: `[⚠️] \`${user.User.tag}\` (\`${user.User.id}\`) has been warned for: \`${warning.message}\` by <@!${warning.moderatorID}>`
+            content: `[⚠️] \`${user.User.tag}\` (\`${user.User.id}\`) has been warned for: \`${warning.message}\` by <@!${moderator}>`
         }).catch(console.log);
     }
 
-    async log_ban(user: NetworkUser, reason: string, modID: string) {
+    async log_ban(user: NetworkUser, reason: string, modID: string | null) {
         const channel = this.client.channels.cache.get(LOGGER_CHANNEL) || await this.client.channels.fetch(LOGGER_CHANNEL).catch(console.log);
 
         if (!channel || !channel.send) {
             return;
         }
 
+        const moderator = modID ? modID : "829383622439206993"
+
         channel.send({
-            content: `[🔨] \`${user.User.tag}\` (\`${user.User.id}\`) has been banned for: \`${reason}\` by <@!${modID}>`
+            content: `[🔨] \`${user.User.tag}\` (\`${user.User.id}\`) has been banned for: \`${reason}\` by <@!${moderator}>`
         }).catch(console.log);
     }
 
-    async log_unban(user: NetworkUser, modID: string) {
+    async log_unban(user: NetworkUser, modID: string | null) {
         const channel = this.client.channels.cache.get(LOGGER_CHANNEL) || await this.client.channels.fetch(LOGGER_CHANNEL).catch(console.log);
 
         if (!channel || !channel.send) {
             return;
         }
 
+        const moderator = modID ? modID : "829383622439206993"
+
         channel.send({
-            content: `[🔨] \`${user.User.tag}\` (\`${user.User.id}\`) has been unbanned by <@!${modID}>`
+            content: `[🔨] \`${user.User.tag}\` (\`${user.User.id}\`) has been unbanned by <@!${moderator}>`
         }).catch(console.log);
     }
 
-    async log_gBan(guild: NetworkGuild, reason: string, modID: string) {
+    async log_gBan(guild: NetworkGuild, reason: string, modID: string | null) {
         const channel = this.client.channels.cache.get(LOGGER_CHANNEL) || await this.client.channels.fetch(LOGGER_CHANNEL).catch(console.log);
 
         if (!channel || !channel.send) {
             return;
         }
 
+        const moderator = modID ? modID : "829383622439206993"
+
         channel.send({
-            content: `[🔨] The guild \`${guild.Guild.name}\` (\`${guild.Guild.id}\`) has been banned for: \`${reason}\` by <@!${modID}>`
+            content: `[🔨] The guild \`${guild.Guild.name}\` (\`${guild.Guild.id}\`) has been banned for: \`${reason}\` by <@!${moderator}>`
         }).catch(console.log);
     }
 
-    async log_gUnban(guild: NetworkGuild, modID: string) {
+    async log_gUnban(guild: NetworkGuild, modID: string | null) {
         const channel = this.client.channels.cache.get(LOGGER_CHANNEL) || await this.client.channels.fetch(LOGGER_CHANNEL).catch(console.log);
 
         if (!channel || !channel.send) {
             return;
         }
+        
+        const moderator = modID ? modID : "829383622439206993"
 
         channel.send({
-            content: `[🔨] The guild \`${guild.Guild.name}\` (\`${guild.Guild.id}\`) has been unbanned by <@!${modID}>`
+            content: `[🔨] The guild \`${guild.Guild.name}\` (\`${guild.Guild.id}\`) has been unbanned by <@!${moderator}>`
         }).catch(console.log);
     }
 }
