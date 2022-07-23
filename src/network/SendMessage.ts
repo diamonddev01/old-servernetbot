e269d28964e01059bb2c0f2c093af4fe36fab388
diamonddev01 <thediamondgamer331@outlook.com>
2022-07-23 03:38:31 +0100

    Finished at remote location, moving back to desktop pc

--- src/network/SendMessage.ts
+++ src/network/SendMessage.ts
@@ -2,9 +2,11 @@ import { Message } from "discord.js";
 import { Client } from "../modified";
 import { NetworkSend } from "./__send";
 import { scan } from "./functions/scanMessage";
+import { NetworkUser } from "../classes/Network_User";
 
 export function CustomNetworkMessage(client: Client, message: Message) {
-    //
+    const user = new NetworkUser(message.author);
+    const cnt = message.content;
 
     //NetworkSend(client, message, data, webhookOpts);
 }
