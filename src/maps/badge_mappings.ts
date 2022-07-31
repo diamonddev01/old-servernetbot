cf3c5860c59532020b166f6e0f5291aacd0ddd98
Oreo <thediamondgamer331@gmail.com>
2022-07-31 02:41:33 +0100

    Add all the badge mappings

---
+++ src/maps/badge_mappings.ts
@@ -0,0 +1,80 @@
+// Main badges
+export const milestone = '<:MessageMilestone:1000123185271935088>';
+export const affiliate = '<:affiliate:969719310274686996>';
+export const contributor = '<:contributor:969719310140461108>';
+export const developer = '<:developer:969719310283075694>';
+export const ex_staff = '<:exStaff:969719310333407242>';
+export const sr_mod = '<:globalModerator:969719310396293120>';
+export const manager = '<:manager:969719310304043008>';
+export const moderator = '<:moderator:969719310115303525>';
+export const official = '<:officialServer:969719310337585152>';
+export const partner = '<:partner:969719310324998164>';
+export const revolt = '<:revolt:969719310333394974>';
+export const staff = '<:staff:969719310350159933>';
+export const verified = '<:verified:969719310350159934>';
+
+// Message Badges
+export const m_100 = '<:100:975218956690423810>';
+export const m_250 = '<:250:975218956904312882>';
+export const m_500 = '<:500:975218956984025118>';
+export const m_750 = '<:750:975218956770111549>';
+export const m_1000 = '<:1k:975218957021761677>';
+export const m_2500 = '<:2k:975218956837224508>';
+export const m_5000 = '<:5k:975218956958826576>';
+export const m_10000 = '<:10k:975218956686196808>';
+
+// Ranked Badges
+export const rk_b1 = '<:bronze1:975221986882121768>';
+export const rk_b2 = '<:bronze2:975221986529787954>';
+export const rk_b3 = '<:bronze3:975221986064220162>';
+export const rk_s1 = '<:silver1:975222022068125736>';
+export const rk_s2 = '<:silver2:975222021883576380>';
+export const rk_s3 = '<:silver3:975222022076526642>';
+export const rk_g1 = '<:gold1:975222061129683014>';
+export const rk_g2 = '<:gold2:975222061242929162>';
+export const rk_g3 = '<:gold3:975222061259685929>';
+export const rk_p1 = '<:plat1:975222101797650473>';
+export const rk_p2 = '<:plat2:975222101961236510>';
+export const rk_p3 = '<:plat3:975222102129004595>';
+
+// Bagde to ID
+export const badgeToId = {
+    // Main badges
+    '<:MessageMilestone:1000123185271935088>': 'milestone',
+    '<:affiliate:969719310274686996>': 'affiliate',
+    '<:contributor:969719310140461108>': 'contributor',
+    '<:developer:969719310283075694>': 'developer',
+    '<:exStaff:969719310333407242>': 'ex_staff',
+    '<:globalModerator:969719310396293120>': 'sr_mod',
+    '<:manager:969719310304043008>': 'manager',
+    '<:moderator:969719310115303525>': 'moderator',
+    '<:officialServer:969719310337585152>': 'official',
+    '<:partner:969719310324998164>': 'partner',
+    '<:revolt:969719310333394974>': 'revolt',
+    '<:staff:969719310350159933>': 'staff',
+    '<:verified:969719310350159934>': 'verified',
+
+    // Message Badges
+    '<:100:975218956690423810>': 'm_100',
+    '<:250:975218956904312882>': 'm_250',
+    '<:500:975218956984025118>': 'm_500',
+    '<:750:975218956770111549>': 'm_750',
+    '<:1k:975218957021761677>': 'm_1000',
+    '<:2k:975218956837224508>': 'm_2500',
+    '<:5k:975218956958826576>': 'm_5000',
+    '<:10k:975218956686196808>': 'm_10000',
+
+    // Ranked Badges
+    '<:bronze1:975221986882121768>': 'rk_b1',
+    '<:bronze2:975221986529787954>': 'rk_b2',
+    '<:bronze3:975221986064220162>': 'rk_b3',
+    '<:silver1:975222022068125736>': 'rk_s1',
+    '<:silver2:975222021883576380>': 'rk_s2',
+    '<:silver3:975222022076526642>': 'rk_s3',
+    '<:gold1:975222061129683014>': 'rk_g1',
+    '<:gold2:975222061242929162>': 'rk_g2',
+    '<:gold3:975222061259685929>': 'rk_g3',
+    '<:plat1:975222101797650473>': 'rk_p1',
+    '<:plat2:975222101961236510>': 'rk_p2',
+    '<:plat3:975222102129004595>': 'rk_p3'
+}
