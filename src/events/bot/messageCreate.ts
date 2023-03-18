32a2f852e3e7b43e2085893467dcc33624d03211
diamonddev01 <thediamondgamer331@outlook.com>
2023-03-18 15:56:15 +0000

    Format issues

--- src/events/bot/messageCreate.ts
+++ src/events/bot/messageCreate.ts
@@ -5,18 +5,18 @@ import { networkMessage } from "../../network/networkMessage";
 
 export function event(client: Client<true>, message: Message): void {
     if (message.author.bot) return;
-    if(!message.content.toLowerCase().startsWith(Prefix)) return serverentLogic(client, message)
+    if (!message.content.toLowerCase().startsWith(Prefix)) return serverentLogic(client, message)
 
     const args = message.content.toLowerCase().slice(Prefix.length).trim().split(/ + /g);
     const command = args.shift();
 
     // Servernet logic
-    if(!command) return serverentLogic(client, message);
+    if (!command) return serverentLogic(client, message);
     const cmd = client.commands.get(command) || client.commands.find(c => c.aliases && c.aliases.includes(command));
     if (cmd) {
         if (!cmd.messageEnabled) return;
-        if(client.channelIdsCache.includes(message.channel.id)) {
-            message.channel.send({content: "You cannot use a command on a network channel"}).catch();
+        if (client.channelIdsCache.includes(message.channel.id)) {
+            message.channel.send({ content: "You cannot use a command on a network channel" }).catch();
             return;
         }
         cmd.events.message(client, message, args);
@@ -26,7 +26,7 @@ export function event(client: Client<true>, message: Message): void {
 }
 
 function serverentLogic(client: Client<true>, message: Message): void {
-    if(client.channelIdsCache.includes(message.channel.id)) {
+    if (client.channelIdsCache.includes(message.channel.id)) {
         networkMessage(client, message);
     }
 
